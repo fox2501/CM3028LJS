fb18aaa2b1a446933d1a56c78a812ad24180c455
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-10-10 15:43:00 +0100

    amended name

---
+++ Week2/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liams
+ * Date: 10/10/2016
+ * Time: 15:40
+ */
+?>
+<h1>Marvel Movies</h1>
+<p1>
+    <li><a href = "allMovies.php">All Movies</a></li>
+    <li><a href = "marvelMovies.php"> Marvel Movies</a></li>
+    <li><a href = "after2010.php">Marvel Films After 2010</a></li>
+    <li><a href = "allXMen.php">X-Men Films</a></li>
+</p1>
