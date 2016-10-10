33f61aea4f3c68a1243a22da4eb817718b0658af
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-10-10 15:40:06 +0100

    amended name

--- Week2/allXMen.php
+++ Week2/allXMen.php
@@ -16,7 +16,7 @@ if($db->connect_errno){
     die('ConnectFailed['.$db->connect_error.']');
 }
 
-$sql_query = "SELECT * FROM marvelmovies WHERE title LIKE '*X-Men*'";
+$sql_query = "SELECT * FROM marvelmovies WHERE title LIKE '%X-Men%'";
 
 $result=$db->query($sql_query);
 
