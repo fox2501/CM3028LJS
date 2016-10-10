b4484077a4eddca35a3f13d7df0634a41e2264e9
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-10-10 15:37:26 +0100

    amended name

---
+++ Week2/allXMen.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liams
+ * Date: 10/10/2016
+ * Time: 15:36
+ */
+$db = new mysqli(
+    'eu-cdbr-azure-west-a.cloudapp.net',
+    'b9cfe9e452edf8',
+    '26a3bb97',
+    'ljstewdb'
+);
+//test if connection was established and print any errors
+if($db->connect_errno){
+    die('ConnectFailed['.$db->connect_error.']');
+}
+
+$sql_query = "SELECT * FROM marvelmovies WHERE title LIKE 'X-Men'";
+
+$result=$db->query($sql_query);
+
+while($row = $result->fetch_array()){
+    $movieTitle = $row['title'];
+    $yearReleased = $row['yearReleased'];
+    $productionStudio = $row['productionStudio'];
+    $notes = $row['notes'];
+    echo "<p>".$movieTitle." " .$yearReleased." " .$productionStudio. " ". $notes."</p>";
+}
+
+$result->close();
+
+$db->close();
