22eadfb9f9a09f405faff3a490ee4dc34010bd79
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-10-17 14:04:56 +0100

    amended name

---
+++ Week3/helloPrinter.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liams
+ * Date: 17/10/2016
+ * Time: 13:44
+ */
+$query = $_GET["planet"];
+echo "Hello <b>" .planet. "</b>";
---
+++ Week3/helloSelector.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+
+</html>
+
+<p> <a href="http://redeplotlswebapp.azurewebsites.net/Week3/helloPrinter.php?planet=Earth">Earth</a>
+    <a href="http://redeplotlswebapp.azurewebsites.net/Week3/helloPrinter.php?planet=Mars">Mars</a>
+    <a href="http://redeplotlswebapp.azurewebsites.net/Week3/helloPrinter.php?planet=Uranus">Uranus</a>
+</p>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liams
+ * Date: 17/10/2016
+ * Time: 13:42
+ */
+
+
+
