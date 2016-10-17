78e8a10b87f647799777f8c276e06f52b385396a
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-10-17 14:29:08 +0100

    amended name

--- Week3/newShield.php
+++ Week3/newShield.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <form action="shieldResponse.php" method ="post">
-    <label>Forename</label><input type="text" name="forename"><br>
+    <label>Forename</label><input type="text" name="forename"><br><br>
     <label>Surname</label><input type="text" name="surname"><br>
     <label>Gender</label><br><input type="radio" name="gender" value ="Male" checked>Male<br>
                          <input type="radio" name="gender" value ="Female">Female<br>
