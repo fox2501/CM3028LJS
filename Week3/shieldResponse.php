d608e82e2a6e92455bbb553350a8f78ef7e42565
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-10-17 14:36:20 +0100

    amended name

--- Week3/shieldResponse.php
+++ Week3/shieldResponse.php
@@ -5,3 +5,10 @@
  * Date: 17/10/2016
  * Time: 14:31
  */
+
+$forename =$_POST["forename"];
+$surname =$_POST["surname"];
+$gender =$_POST["gender"];
+$superPower =$_POST["superPower"];
+
+echo $forename.$surname. " is " . $gender . " and has the Super Power of " . $superPower;
