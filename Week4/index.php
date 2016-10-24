d3a4ca1a08f0ccb5d5307f41ed028c0eee11ce94
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-10-24 14:34:10 +0100

    amended name

--- Week4/index.php
+++ Week4/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <form action="checkLogin.php" method ="post">
     <label>Username</label><input type="text" name="username"><br><br>
-    <label>Password</label><input type="text" name="password"><br>
+    <label>Password</label><input type="password" name="password"><br>
     <input type ="submit" value ="submit">
 
 </form>
