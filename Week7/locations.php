facabead3516f0b5a5eeadf063381af895247fe6
Liam Stewart <liamstewart777@hotmail.co.uk>
2016-11-07 14:40:10 +0000

    amended name

--- Week7/locations.php
+++ Week7/locations.php
@@ -1,7 +1,17 @@
 <?php
 
 // Start XML file, create parent node
+$db = new mysqli(
+    'eu-cdbr-azure-west-a.cloudapp.net',
+    'bef6fb84bef4ce',
+    'd5326f0f',
+    'cm3038ljsdb'
+);
+//test if connection was established and print any errors
+if($db->connect_errno){
+    die('ConnectFailed['.$db->connect_error.']');
 
+}
 
 $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
